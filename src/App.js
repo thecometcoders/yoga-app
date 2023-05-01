2d230f7c0d90f365f20631341ccfa4b018c9a0b8
Andrew Heyer <andrewbheyer@gmail.com>
2023-05-01 18:46:06 -0400

    rebasing on main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import Home from "./routes/home/home.component";
+=======
+import logo from "./logo.svg";
+import "./App.css";
+>>>>>>> 02b8743 (added prettier configuration and formated project)
 
 function App() {
   return (
