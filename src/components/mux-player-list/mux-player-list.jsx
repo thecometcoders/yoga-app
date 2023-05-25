7d46bd96cf4d7da7ac6ec60e76cb7aabc47bebed
Andrew Heyer <andrewbheyer@gmail.com>
2023-05-25 17:04:04 -0400

    Creating a hardcoded component for audio files. Needs to be dynamically pulling content.

---
+++ src/components/mux-player-list/mux-player-list.jsx
@@ -0,0 +1,42 @@
+import MuxPlayer from "@mux/mux-player-react";
+import { Container, Item } from "./mux-player-list.styles";
+
+export default function MuxPlayerList() {
+  return (
+    <Container>
+      <h1>Yoga Audios</h1>
+      <p>
+        Experience a practice that is reinforcing and supportive of wherever you
+        choose to focus your energy. To enjoy these audios all you need is a
+        quite place to practice, a yoga mat and speaker.
+      </p>
+      <Item>
+        <MuxPlayer
+          title={"This is the title"}
+          playbackId={"AKeCXTzf4bGfFd01023JAn671dI9y02P91bVMb5CMx2pcs"}
+          streamType={"on-demand"}
+          audio={true}
+          metadata={{ video_id: "videoId20", video_title: "Title: Audio Only" }}
+        />
+      </Item>
+      <Item>
+        <MuxPlayer
+          title={"This is the title"}
+          playbackId={"AKeCXTzf4bGfFd01023JAn671dI9y02P91bVMb5CMx2pcs"}
+          streamType={"on-demand"}
+          audio={true}
+          metadata={{ video_id: "videoId20", video_title: "Title: Audio Only" }}
+        />
+      </Item>
+      <Item>
+        <MuxPlayer
+          title={"This is the title"}
+          playbackId={"AKeCXTzf4bGfFd01023JAn671dI9y02P91bVMb5CMx2pcs"}
+          streamType={"on-demand"}
+          audio={true}
+          metadata={{ video_id: "videoId20", video_title: "Title: Audio Only" }}
+        />
+      </Item>
+    </Container>
+  );
+}
